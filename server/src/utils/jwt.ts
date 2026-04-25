72a86439908b383485670fb8d46e8f3396e2f8fb
Beka <sskahabaev@mail.ru>
2026-04-25 17:18:06 +0500

    fix jwt util

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -2,5 +2,5 @@ import jwt from "jsonwebtoken";
 import { env } from "../config/env";
 
 export function signAccessToken(payload: { userId: string; role: string }) {
-  return jwt.sign(payload, env.accessSecret, { expiresIn: "7d" });
+  return jwt.sign(payload, env.jwtAccessSecret, { expiresIn: "7d" });
 }
